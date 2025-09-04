abe8134214c80cf15aa150c11f1c05281a589e2a
Ahmed Waleed <62625165+BoiOG@users.noreply.github.com>
2025-09-04 14:43:27 -0600

    files uploaded

--- top.v
+++ top.v
@@ -3,12 +3,14 @@ module top(
     output [15:0] led
 );
 
-// Enter Your equation here
+    // Enter your equation here
 
+    
     genvar i;
     generate
-    for(i = 0; i < 16; i = i + 1)
-        assign led[i] = sw[i];
-    endgenerate
+        for(i = 0; i < 16; i = i + 1)
+            assign led[i] = sw[i];
+     endgenerate
+    
 
 endmodule
